91d7fe6c4353a68c2060b6d2c64f363ad99dacb6
Supitcha Jampathong (NTL) <CF303956@tidlor.com>
2022-06-08 15:36:21 +0700

    add class calgrade

---
+++ Grade/CalGrade.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grade
+{
+    public class CalGrade
+    {
+        private int Score { get; set; }
+
+        public CalGrade(int score) {
+            this.Score = score;
+        
+        }
+
+        public string Excute() {
+            var response = "";
+           
+        
+
+            if (Score >= 101)
+            {
+               response = "ERROR";
+            }
+            else if (Score >= 80)
+            {
+                response = "A";
+            }
+            else if (Score >= 70)
+            {
+                response = "B";
+            }
+            else if (Score >= 60)
+            {
+                response = "C";
+
+            }
+            else if (Score >= 50)
+            {
+                response = "D";
+            }
+            else if (Score <= 49)
+            {
+                response = "F";
+
+            }
+            return response;
+
+        }
+
+       
+    }
+
+    
+}
--- Grade/Program.cs
+++ Grade/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Grade;
+
 Console.WriteLine("-----------**********-----------\n");
 Console.WriteLine("Let's calculate the grade!!\n");
 
@@ -8,32 +10,35 @@ var score = Console.ReadLine();
 
 var scoregrade = int.Parse(score);
 
+var calcgrade = new CalGrade(scoregrade);
+var grade = calcgrade.Excute();
+Console.WriteLine($"Your grade : {grade}\n");
 
-if (scoregrade >= 101) {
-    Console.WriteLine("--- Error ---\n");
-} 
-else if (scoregrade >= 80)
-{
-    Console.WriteLine("Your grade : A\n");
-}
-else if (scoregrade >= 70)
-{
-    Console.WriteLine("Your grade : B\n");
-}
-else if (scoregrade >= 60)
-{
-    Console.WriteLine("Your grade : C\n");
+//if (scoregrade >= 101) {
+//    Console.WriteLine("--- Error ---\n");
+//} 
+//else if (scoregrade >= 80)
+//{
+//    Console.WriteLine("Your grade : A\n");
+//}
+//else if (scoregrade >= 70)
+//{
+//    Console.WriteLine("Your grade : B\n");
+//}
+//else if (scoregrade >= 60)
+//{
+//    Console.WriteLine("Your grade : C\n");
 
-}
-else if (scoregrade >= 50)
-{
-    Console.WriteLine("Your grade : D\n");
-}
-else if (scoregrade <= 49)
-{
-    Console.WriteLine("Your fail T^T\n");
+//}
+//else if (scoregrade >= 50)
+//{
+//    Console.WriteLine("Your grade : D\n");
+//}
+//else if (scoregrade <= 49)
+//{
+//    Console.WriteLine("Your fail T^T\n");
 
-}
+//}
 
 Console.WriteLine("-----------**********-----------");
 
